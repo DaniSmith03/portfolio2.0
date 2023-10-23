ba4ec0ecc186f4d34fd51a66fb3f8f31f9cb05bf
Dani Smith <danismith03@gmail.com>
2023-10-23 13:59:58 -0500

    project section started

--- script.js
+++ script.js
@@ -65,3 +65,62 @@ document.addEventListener('DOMContentLoaded', function () {
     }
   });
 });
+
+document.addEventListener('DOMContentLoaded', function () {
+  const card = document.querySelector('.about-me-bio .card');
+  const flipButton = document.getElementById('flip-button');
+  const returnButton = document.getElementById('return-button');
+  const image = document.querySelector('.about-me-image img');
+  function flipCard() {
+    card.classList.toggle('flipped');
+    updateCardContent();
+    updateQuote();
+  }
+
+  function updateCardContent() {
+    if (card.classList.contains('flipped')) {
+      image.src = './images/daniBubbles.png';
+      document
+        .querySelector('.original-img')
+        .classList.replace('original-img', 'alt');
+    } else {
+      image.src = './images/daniHeadshot.png';
+      document.querySelector('.alt').classList.replace('alt', 'original-img');
+    }
+  }
+
+  function updateQuote() {
+    if (card.classList.contains('flipped')) {
+      document.querySelector('.block-quote').innerHTML =
+        "“ Be who you are and say what you feel, because those who mind don't matter, and those who matter don't mind. ”";
+      document.querySelector('.author-quote').innerHTML = '-Dr. Seuss';
+    } else {
+      document.querySelector('.block-quote').innerHTML =
+        '“ All Limitations Are Self Imposed. ”';
+      document.querySelector('.author-quote').innerHTML =
+        '-Oliver Wendell Holmes';
+    }
+  }
+
+  flipButton.addEventListener('click', flipCard);
+  returnButton.addEventListener('click', flipCard);
+});
+
+//function to raise the block quote with scroll
+window.addEventListener('scroll', function () {
+  // Get the position of the "about-me" section
+  const aboutMeSection = document.querySelector('.about-me');
+  const aboutMeSectionRect = aboutMeSection.getBoundingClientRect();
+
+  // Check if the section is in the viewport
+  if (
+    aboutMeSectionRect.top < window.innerHeight &&
+    aboutMeSectionRect.bottom > 0
+  ) {
+    // Apply the "raised" class to the block quote
+    document.querySelector('.block-quote-container').classList.add('raised');
+  } else {
+    // Remove the "raised" class when the section is not in view
+    document.querySelector('.block-quote-container').classList.remove('raised');
+  }
+});
